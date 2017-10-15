e5316fbee90ca4b9f9cb86b9c311f2bd9a5a3de9
Biswas <biswasb007@gmail.com>
2017-10-15 19:58:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a remind for me
+
+Opencv lives in a conda env called cv
+activate it by
+source activate cv
---
+++ p1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+from fbrecog import FBRecog
+from config import *
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    path = 'frame.jpg'
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    k = cv2.waitKey(1)
+    if (k==1048603):    # Esc key to stop
+        break
+    elif (k==-1):  # normally -1 returned,so don't print it
+        continue
+    elif (k==1048691): # S key to save
+        cv2.imwrite(path, frame)
+    else:
+        print(k) # else print its value
+    # Instantiate the recog class
+    recog = FBRecog(access_token, cookie, fb_dtsg)
+    # Recog class can be used multiple times with different paths
+    print(recog.recognize(path))
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
